c651bd399b1738d07afde4704f17331508eb8f52
IsrarAhmed919 <ahmedisrar804@gmail.com>
2025-09-22 16:14:39 +0500

    Updated tf file

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
+
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
 
 data "azurerm_client_config" "current" {}
 
